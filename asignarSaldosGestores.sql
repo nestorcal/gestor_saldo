77d5f024ff0f33b650b73426726bc75d109e17a1
nestorcal <nescalh@gmail.com>
2024-10-03 17:56:43 -0500

    Procedimiento almacenado que realiza asignacion de saldos a gestores y pone valores en tabla temporal #TempAsignaciones

--- asignarSaldosGestores.sql
+++ asignarSaldosGestores.sql
@@ -1,8 +1,70 @@
-﻿CREATE OR ALTER PROCEDURE GestorSaldo.AsignarSaldosGestores
+﻿CREATE OR ALTER PROCEDURE AsignarSaldosGestores
 AS
-    BEGIN
-        
-        TRUNCATE TABLE Asignaciones;
-        DECLARE @TotalSaldos INT, @TotalGestores
-        
-    END
+BEGIN
+    DECLARE @TotalGestores INT;
+    DECLARE @TotalSaldos INT;
+    DECLARE @SaldoIndex INT = 1;
+    DECLARE @GestorIndex INT =1;
+    DECLARE @GestorId INT;
+    DECLARE @GestorNombre VARCHAR(50);
+    DECLARE @SaldoId INT;
+    DECLARE @Saldo DECIMAL(18,2);
+    DECLARE @Iteraciones INT;
+    DECLARE @IteracionActual INT = 1;
+
+    -- Crear tabla temporal con saldos ordenados
+    CREATE TABLE #TempAsignaciones (
+                                       Gestores VARCHAR(50),
+                                       Saldos DECIMAL(10,2)
+    );
+
+    CREATE TABLE #TempSaldos (
+                                 ID INT IDENTITY(1,1) PRIMARY KEY,
+                                 Saldo DECIMAL(10,2)
+    );
+
+    INSERT INTO #TempSaldos (Saldo)
+    SELECT Saldos
+    FROM GestorSaldo.dbo.Saldos
+    ORDER BY Saldos DESC;
+
+    SELECT @TotalGestores = COUNT(*) FROM GestorSaldo.dbo.Gestores;
+    SELECT @TotalSaldos = COUNT(*) FROM GestorSaldo.dbo.Saldos;
+    SET @Iteraciones = CEILING(CAST(@TotalSaldos AS FLOAT) / @TotalGestores);
+
+    DECLARE gestorCursor CURSOR FOR
+        SELECT ID, Gestores
+        FROM GestorSaldo.dbo.Gestores;
+
+    WHILE @IteracionActual <= @Iteraciones
+        BEGIN
+            OPEN gestorCursor;
+            SET @GestorIndex = 1;
+
+            -- Asignación saldos a cada gestor
+            WHILE @GestorIndex <= @TotalGestores AND @SaldoIndex <=@TotalSaldos
+                BEGIN
+                    FETCH NEXT FROM gestorCursor INTO @GestorId, @GestorNombre;
+                    IF @@FETCH_STATUS != 0
+                        BEGIN
+                            CLOSE gestorCursor;
+                            OPEN gestorCursor;
+                            FETCH NEXT FROM gestorCursor INTO @GestorId, @GestorNombre;
+                        END
+
+                    SELECT @Saldo = Saldo FROM #TempSaldos WHERE ID = @SaldoIndex;
+
+                    INSERT INTO #TempAsignaciones (Gestores, Saldos)
+                    VALUES (@GestorNombre, @Saldo);
+
+                    SET @GestorIndex = @GestorIndex + 1;
+                    SET @SaldoIndex= @SaldoIndex + 1;
+                END
+
+            CLOSE gestorCursor;
+
+            SET @IteracionActual = @IteracionActual + 1;
+        end
+    DEALLOCATE gestorCursor;
+    select * from #TempAsignaciones
+END;
--- crearBdTalas.sql
+++ crearBdTalas.sql
@@ -19,7 +19,7 @@ IF NOT EXISTS (SELECT * from sysobjects WHERE name='Saldos')
     BEGIN
         CREATE TABLE Saldos(
             ID INT IDENTITY(1,1) PRIMARY KEY,
-            Monto DECIMAL(10,2) NOT NULL 
+            Saldos DECIMAL(10,2) NOT NULL 
         );
         PRINT 'Tabla Saldos creada';
     END
@@ -33,7 +33,7 @@ IF NOT EXISTS (SELECT * from sysobjects WHERE name='Gestores')
     BEGIN
         CREATE TABLE Gestores(
             ID INT IDENTITY(1,1) PRIMARY KEY,
-            Nombre VARCHAR(50) NOT NULL
+            Gestores VARCHAR(50) NOT NULL
         );
         PRINT 'Tabla Gestores creada';
     END
@@ -43,33 +43,17 @@ ELSE
     END
 GO
 
-IF NOT EXISTS (SELECT * from sysobjects WHERE name='Asignaciones')
-    BEGIN
-        CREATE TABLE Asignaciones(
-            ID INT IDENTITY(1,1) PRIMARY KEY,
-            GestorID INT NOT NULL,
-            SaldoID INT NOT NULL,
-            FOREIGN KEY (GestorID) REFERENCES Gestores(ID),
-            FOREIGN KEY (SaldoID) REFERENCES Saldos(ID)
-        );
-        PRINT 'Tabla Asignaciones creada';
-    END
-ELSE
-    BEGIN
-        PRINT 'La Tabla Asignaciones ya existe';
-    END
-GO
-
 -- Insertar Datos
 IF NOT EXISTS (SELECT TOP 1 1 FROM Saldos)
     BEGIN
-        INSERT INTO Saldos(Monto) VALUES (2277), (3953), (4726), (1414), (627), 
-        (1784), (1634), (3958), (2156), (1347), (2166), (820(), (2325),
+        INSERT INTO Saldos(Saldos) VALUES (2277), (3953), (4726), (1414), (627), 
+        (1784), (1634), (3958), (2156), (1347), (2166), (820), (2325),
         (3613), (2389), (4130), (2007), (3027), (2591), (3940), (3888),
         (2975), (4470), (2291), (3393), (3588), (3286), (2293), (4353),
         (3315), (4900), (794), (4424), (4505), (2643), (2217), (4193),
         (2893), (4120), (3352), (2355), (3219), (3064), (4893), (272),
         (1299), (4725), (1900), (4927), (4011);
+        PRINT 'Datos insertados en Saldos';
     END
 ELSE
     BEGIN
@@ -79,9 +63,10 @@ ELSE
 -- DAtos Gestores
 IF NOT EXISTS (SELECT TOP 1 1 FROM Gestores)
     BEGIN
-        INSERT INTO Gestores(Nombre) VALUES ('Gestor1'), ('Gestor2'), ('Gestor3'), 
+        INSERT INTO Gestores(Gestores) VALUES ('Gestor1'), ('Gestor2'), ('Gestor3'), 
         ('Gestor4'), ('Gestor5'), ('Gestor6'), ('Gestor7'), ('Gestor8'),
         ('Gestor9'), ('Gestor10');
+        PRINT 'Datos insertados en Gestores';
     END
 ELSE
     BEGIN
