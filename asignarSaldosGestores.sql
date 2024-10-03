aec9699245526e8c284721737290fe411ec942cc
nestorcal <nescalh@gmail.com>
2024-10-03 12:00:06 -0500

    Correcciones

---
+++ asignarSaldosGestores.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR ALTER PROCEDURE GestorSaldo.AsignarSaldosGestores
+AS
+    BEGIN
+        
+        TRUNCATE TABLE Asignaciones;
+        DECLARE @TotalSaldos INT, @TotalGestores
+        
+    END
